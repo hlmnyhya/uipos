12b56812b285a302b1b93c0227541eb7ecfc88fe
Hilmanadi Yahya <hlmnyhya99@gmail.com>
2023-05-31 07:42:23 +0800

    print done

--- lib/transaction/print.dart
+++ lib/transaction/print.dart
@@ -11,7 +11,13 @@ import 'package:http/http.dart' as http;
 import 'package:uipos2/transaction/transaction_screen.dart';
 import 'dart:convert';
 import '../cappbar.dart';
-// import 'package:esc_pos_bluetooth/esc_pos_bluetooth.dart';
+import 'package:blue_thermal_printer/blue_thermal_printer.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:blue_thermal_printer/blue_thermal_printer.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uipos2/transaction/testprint.dart';
 
 class Print extends StatefulWidget {
   const Print({super.key});
@@ -21,6 +27,107 @@ class Print extends StatefulWidget {
 }
 
 class _PrintState extends State<Print> {
+  BlueThermalPrinter bluetooth = BlueThermalPrinter.instance;
+
+  List<BluetoothDevice> _devices = [];
+  BluetoothDevice? _device;
+  bool _connected = false;
+  TestPrint testPrint = TestPrint();
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    // TODO here add a permission request using permission_handler
+    // if permission is not granted, kzaki's thermal print plugin will ask for location permission
+    // which will invariably crash the app even if user agrees so we'd better ask it upfront
+
+    // var statusLocation = Permission.location;
+    // if (await statusLocation.isGranted != true) {
+    //   await Permission.location.request();
+    // }
+    // if (await statusLocation.isGranted) {
+    // ...
+    // } else {
+    // showDialogSayingThatThisPermissionIsRequired());
+    // }
+    bool? isConnected = await bluetooth.isConnected;
+    List<BluetoothDevice> devices = [];
+    try {
+      devices = await bluetooth.getBondedDevices();
+    } on PlatformException {}
+
+    bluetooth.onStateChanged().listen((state) {
+      switch (state) {
+        case BlueThermalPrinter.CONNECTED:
+          setState(() {
+            _connected = true;
+            print("bluetooth device state: connected");
+          });
+          break;
+        case BlueThermalPrinter.DISCONNECTED:
+          setState(() {
+            _connected = false;
+            print("bluetooth device state: disconnected");
+          });
+          break;
+        case BlueThermalPrinter.DISCONNECT_REQUESTED:
+          setState(() {
+            _connected = false;
+            print("bluetooth device state: disconnect requested");
+          });
+          break;
+        case BlueThermalPrinter.STATE_TURNING_OFF:
+          setState(() {
+            _connected = false;
+            print("bluetooth device state: bluetooth turning off");
+          });
+          break;
+        case BlueThermalPrinter.STATE_OFF:
+          setState(() {
+            _connected = false;
+            print("bluetooth device state: bluetooth off");
+          });
+          break;
+        case BlueThermalPrinter.STATE_ON:
+          setState(() {
+            _connected = false;
+            print("bluetooth device state: bluetooth on");
+          });
+          break;
+        case BlueThermalPrinter.STATE_TURNING_ON:
+          setState(() {
+            _connected = false;
+            print("bluetooth device state: bluetooth turning on");
+          });
+          break;
+        case BlueThermalPrinter.ERROR:
+          setState(() {
+            _connected = false;
+            print("bluetooth device state: error");
+          });
+          break;
+        default:
+          print(state);
+          break;
+      }
+    });
+
+    if (!mounted) return;
+    setState(() {
+      _devices = devices;
+    });
+
+    if (isConnected == true) {
+      setState(() {
+        _connected = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,10 +136,196 @@ class _PrintState extends State<Print> {
         breadcrumbItem: 'Print',
         breadcrumbItem2: 'Payment',
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Text('Print'),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: ListView(
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(width: 10),
+                const Text(
+                  'Device :',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Colors.grey[200],
+                    ),
+                    child: DropdownButton(
+                      items: _getDeviceItems(),
+                      onChanged: (BluetoothDevice? value) =>
+                          setState(() => _device = value),
+                      value: _device,
+                      underline: SizedBox(), // Remove the default underline
+                      isExpanded:
+                          true, // Expand the dropdown to fill the available width
+                      icon: Icon(Icons.arrow_drop_down), // Add a dropdown icon
+                      dropdownColor:
+                          Colors.white, // Set dropdown background color
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16), // Set dropdown text style
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 50),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                      minimumSize: Size(150, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                      backgroundColor: Color.fromARGB(255, 228, 171, 0),
+                      fixedSize: Size(150, 50),
+                    ),
+                    onPressed: () {
+                      initPlatformState();
+                    },
+                    child: const Text(
+                      'Refresh',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontFamily: 'Poppins'),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                      minimumSize: Size(150, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                      backgroundColor: Color.fromARGB(188, 209, 0, 0),
+                      fixedSize: Size(150, 50),
+                    ),
+                    onPressed: _connected ? _disconnect : _connect,
+                    child: Text(
+                      _connected ? 'Disconnect' : 'Connect',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontFamily: 'Poppins'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(40.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                    minimumSize: Size(150, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    backgroundColor: Color.fromARGB(188, 209, 0, 0),
+                    fixedSize: Size(150, 50),
+                  ),
+                  onPressed: () {
+                    testPrint.sample();
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.print, color: Colors.white, size: 20),
+                      SizedBox(width: 5),
+                      Text(
+                        'PRINT RECEIPT',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontFamily: 'Poppins'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
+      ),
+    );
+  }
+
+  List<DropdownMenuItem<BluetoothDevice>> _getDeviceItems() {
+    List<DropdownMenuItem<BluetoothDevice>> items = [];
+    if (_devices.isEmpty) {
+      items.add(DropdownMenuItem(
+        child: Text('NONE'),
+      ));
+    } else {
+      _devices.forEach((device) {
+        items.add(DropdownMenuItem(
+          child: Text(device.name ?? ""),
+          value: device,
+        ));
+      });
+    }
+    return items;
+  }
+
+  void _connect() {
+    if (_device != null) {
+      bluetooth.isConnected.then((isConnected) {
+        if (isConnected == false) {
+          bluetooth.connect(_device!).catchError((error) {
+            setState(() => _connected = false);
+          });
+          setState(() => _connected = true);
+        }
+      });
+    } else {
+      show('No device selected.');
+    }
+  }
+
+  void _disconnect() {
+    bluetooth.disconnect();
+    setState(() => _connected = false);
+  }
+
+  Future show(
+    String message, {
+    Duration duration: const Duration(seconds: 3),
+  }) async {
+    await new Future.delayed(new Duration(milliseconds: 100));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          style: const TextStyle(color: Colors.white),
+        ),
+        duration: duration,
       ),
     );
   }
